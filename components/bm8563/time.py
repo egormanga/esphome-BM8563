ca436c3803b01df010250cbf291c21308c4d5e7f
Egor Vorontsov <sdoregor@sdore.me>
2025-01-27 22:20:14 +0300

    Fixed formatting.

--- components/bm8563/time.py
+++ components/bm8563/time.py
@@ -1,9 +1,11 @@
-import esphome.config_validation as cv
 import esphome.codegen as cg
+import esphome.config_validation as cv
 from esphome import automation
 from esphome.components import i2c, time
 from esphome.const import CONF_ID
 
+
+CODEOWNERS = ('@egormanga',)
 
 DEPENDENCIES = ('i2c',)
 
@@ -14,36 +16,9 @@ BM8563Component = bm8563_ns.class_('BM8563Component', time.RealTimeClock, i2c.I2
 WriteAction = bm8563_ns.class_('WriteAction', automation.Action)
 ReadAction = bm8563_ns.class_('ReadAction', automation.Action)
 
-
 CONFIG_SCHEMA = time.TIME_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(BM8563Component),
 }).extend(i2c.i2c_device_schema(CONF_I2C_ADDR))
-
-
-@automation.register_action(
-    'bm8563.write_time',
-    WriteAction,
-    cv.Schema({
-        cv.GenerateID(): cv.use_id(BM8563Component),
-    }),
-)
-async def bm8563_write_time_to_code(config, action_id, template_arg, args):
-    var = cg.new_Pvariable(action_id, template_arg)
-    await cg.register_parented(var, config[CONF_ID])
-    return var
-
-
-@automation.register_action(
-    'bm8563.read_time',
-    ReadAction,
-    automation.maybe_simple_id({
-        cv.GenerateID(): cv.use_id(BM8563Component),
-    }),
-)
-async def bm8563_read_time_to_code(config, action_id, template_arg, args):
-    var = cg.new_Pvariable(action_id, template_arg)
-    await cg.register_parented(var, config[CONF_ID])
-    return var
 
 
 async def to_code(config):
@@ -52,3 +27,21 @@ async def to_code(config):
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
     await time.register_time(var, config)
+
+
+@automation.register_action('bm8563.write_time', WriteAction, cv.Schema({
+    cv.GenerateID(): cv.use_id(BM8563Component),
+}))
+async def bm8563_write_time_to_code(config, action_id, template_arg, args):
+    var = cg.new_Pvariable(action_id, template_arg)
+    await cg.register_parented(var, config[CONF_ID])
+    return var
+
+
+@automation.register_action('bm8563.read_time', ReadAction, automation.maybe_simple_id({
+    cv.GenerateID(): cv.use_id(BM8563Component),
+}))
+async def bm8563_read_time_to_code(config, action_id, template_arg, args):
+    var = cg.new_Pvariable(action_id, template_arg)
+    await cg.register_parented(var, config[CONF_ID])
+    return var
